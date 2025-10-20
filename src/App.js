64e90b6958853d44a35a11a16614426d1deb305e
박규영 <magic9282m@gmail.com>
2025-10-20 18:55:55 +0900

    feat: add new method doCalculate
    App(in App.js)에 doCalculate 메서드 추가 및 App.run() 메서드 내용 작성
    - App.run에서 Console.readLineSync() 호출한 뒤, 입력받은 문자열을 doCalculate의 인자로 전달
    - doCalculate는 커스텀 구분자 추출 및 이스케이프, 구분자 기준으로 문자열 분리 및 형변환, 구분한 숫자들을 합산하는 일련의 작업을 수행한 뒤 Console.print() 를 통해 출력

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import { Console } from "@woowacourse/mission-utils";
 
 class App {
-  async run() {}
+  async run() {
+    Console.readLineAsync("덧셈할 문자열을 입력해 주세요.").then((line) => {
+      this.doCalculate(line);
+    });
+  }
   extractDelimiter(str) { 
     const regex = "\\/\\/(.+?)\\n";
     const matches = [...str.matchAll(regex)];
@@ -31,6 +36,25 @@ class App {
       sum += int;
     }
     return sum;
+  }
+  doCalculate(str) { 
+    let delimiter = this.extractDelimiter(str);
+    if (delimiter !== "") { 
+      delimiter = this.escapeAllSpecials(delimiter);
+    }
+    // 입력한 문자열에 \n이 포함된 경우, 즉 커스텀 구분자가 존재하는 경우
+    const index = str.indexOf("\\n");
+    // 커스텀 구분자 이후 구간으로 문자열 범위 좁힘 ( 커스텀 구분자 없는 경우 그대로 사용 )
+    str = index >= 0 ? str.substring(index + 2) : str;
+    let arr = [];
+    try {
+      arr = this.splitWithDelimiters(str, delimiter);
+    } catch (e) { 
+      console.error("[ERROR]", e.message);
+      return;
+    }
+    const result = this.sumAllIntegers(arr);
+    Console.print(`결과 : ${result}`);
   }
 }
 
