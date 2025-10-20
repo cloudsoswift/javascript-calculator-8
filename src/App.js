1e7422139601499aad4fd12d7266b801973439b7
박규영 <magic9282m@gmail.com>
2025-10-20 18:06:32 +0900

    feat: add new method escapeAllSpecials
    App(in App.js)에 escapeAllSpecials 메서드 추가
    - 매개변수로 주어진 문자열에 대해 정규 표현식에서 사용되는 특수문자들을 모두 이스케이프 처리

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ class App {
     const matches = [...str.matchAll(regex)];
     return matches.length > 0 ? matches[0][1] : "";
   }
+  escapeAllSpecials(str) { 
+    // 정규 표현식에서 특수한 역할을 수행하는 특수문자들을 모두 이스케이프
+    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  }
 }
 
 export default App;
