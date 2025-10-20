c227de89663d853fd1c00c37e8447dac01da4ab0
박규영 <magic9282m@gmail.com>
2025-10-20 18:25:42 +0900

    feat: add new method splitWithDelimiters
    App(in App.js)에 splitWithDelimiters 메서드 추가
    - 문자열과 커스텀 구분자를 매개변수로 받음
    - 기본 구분자(쉼표, 콜론)와 커스텀 구분자를 이용해 문자열 분리
    - 이후, 분리된 각 문자에 대해 숫자형으로 변환하고, 만약 음수라면 에러 발생

--- src/App.js
+++ src/App.js
@@ -10,6 +10,21 @@ class App {
     // 정규 표현식에서 특수한 역할을 수행하는 특수문자들을 모두 이스케이프
     return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
   }
+  splitWithDelimiters(str, delimiter) {
+    // 문자열(str)에 포함된 각 숫자들을 분리하여 숫자 형태로 보관할 배열 result
+    const result = [];
+    // 쉼표, 콜론, 그리고 사용자가 입력한 커스텀 구분자가 있다면 그를 포함해
+    const delimiter = new RegExp(`,|:${delimiter !== "" ? `|${delimiter}` : ""}`);
+    // 문자열 나눔
+    const split = str.split(delimiter);
+    for (const num of split) { 
+      result.push(Number(num));
+      if (result.at(-1) < 0) { 
+        throw new Error('주어진 문자열에는 구분자와 양수만 포함되어야 합니다. 음수는 포함될 수 없습니다');
+      }
+    }
+    return result;
+  } 
 }
 
 export default App;
