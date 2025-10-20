c0008f969c8bfe3bd46f7a1f25766866f94fbe10
박규영 <magic9282m@gmail.com>
2025-10-20 17:51:00 +0900

    feat: add new method extractDelimiter
    App(in App.js)에 extractDelimiter 메서드 추가
    - 정규식을 이용해 문자열로부터 커스텀 구분자를 추출하는 메서드

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+
 class App {
   async run() {}
+  extractDelimiter(str) { 
+    const regex = "\\/\\/(.+?)\\n";
+    const matches = [...str.matchAll(regex)];
+    return matches.length > 0 ? matches[0][1] : "";
+  }
 }
 
 export default App;
